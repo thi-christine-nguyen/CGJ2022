9c4c34c8c05e4f321e29d8ca78c58b10da64f650
thibaultodor <60348757+thibaultodor@users.noreply.github.com>
2023-01-25 00:23:22 +0100

    Add Alexis Copy

--- Assets/Scripts/UIAppear.cs
+++ Assets/Scripts/UIAppear.cs
@@ -6,18 +6,37 @@ public class UIAppear : MonoBehaviour
 {
     [SerializeField] private Canvas Message;
 
+    private double Yfinal = 255;
+    private bool isColid = false;
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Message.enabled = true;
+            isColid = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player")){
-            Message.enabled = false;
+            isColid = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isColid)
+        {
+            if(Message.transform.position.y < 500){
+                Message.transform.position = Message.transform.position + new Vector3(0,3,0);
+            }
+        }
+        else{
+            if (Message.transform.position.y > -150){
+                Message.transform.position = Message.transform.position - new Vector3(0,3,0);
+            }
         }
     }
 }
